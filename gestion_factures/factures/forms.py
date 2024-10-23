bb19d70b9a9a9ac5d76513f9aebba88e13b08e19
NaahilKDJ <134393859+NaahilKDJ@users.noreply.github.com>
2024-10-23 17:01:30 +0200

    paufinnage de display + modif de facture_detail + ajout nouvel attribut de status de paiement

--- gestion_factures/factures/forms.py
+++ gestion_factures/factures/forms.py
@@ -4,7 +4,7 @@ from factures.models import Facture
 class FactureForm(forms.ModelForm):
     class Meta:
         model = Facture
-        fields = ['client', 'article_name', 'article_quantity', 'ttc', 'ht', 'date']
+        fields = ['client', 'article_name', 'article_quantity', 'ttc', 'ht', 'date', 'paid']
         widgets = {
             'Date'              : forms.DateInput(attrs={'type': 'date', 'class': 'form-control input'}),
             'Client'            : forms.TextInput(attrs={'class': 'form-control input'}),
@@ -12,11 +12,24 @@ class FactureForm(forms.ModelForm):
             'Article Quantity'  : forms.NumberInput(attrs={'class': 'form-control input'}),
             'TTC'               : forms.NumberInput(attrs={'class': 'form-control input', 'step': '0.01'}),
             'HT'                : forms.NumberInput(attrs={'class': 'form-control input', 'step': '0.01'}),
+            'Payment status'    : forms.BooleanField()
         }
 
 
     def clean_ttc(self):
         ttc = self.cleaned_data['ttc']
-        if ttc < 0:
-            raise forms.ValidationError("Le montant TTC ne peut pas être négatif.")
+        
+        if ttc < 0 or ttc==None or ttc=="":
+            raise forms.ValidationError("Le montant TTC ne peut pas être négatif ou vide !")
+        
         return ttc
+    
+    
+    def clean_Client(self):
+        client = self.cleaned_data['client']
+        
+        if client=="" or client==None:
+            raise forms.ValidationError("Veuillez mentionner le nom du client !")
+        
+        return client
+    
--- gestion_factures/factures/models.py
+++ gestion_factures/factures/models.py
@@ -9,6 +9,7 @@ class Facture(models.Model):
     ttc     = models.DecimalField(max_digits=10, decimal_places=2)
     ht      = models.DecimalField(max_digits=10, decimal_places=2)
     date    = models.DateField(default=timezone.now)
+    paid    = models.BooleanField(default=False)
     
     # You can add more fields as needed, such as:
     # numero_facture = models.CharField(max_length=20, unique=True)
